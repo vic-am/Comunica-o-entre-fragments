7efebad8c9e6b4fc929f8c3f94a2f9c62d8564f2
Victor Amaral <vic_amaral_23@hotmail.com>
2020-04-12 08:59:05 -0300

    Métodos onClick melhorados para melhor leitura e manutenção

--- app/src/main/java/com/example/myapplication/fragments/ScrollingFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/ScrollingFragment.java
@@ -16,11 +16,12 @@ import com.example.myapplication.R;
 import com.example.myapplication.interfaces.Comunicador;
 import com.example.myapplication.model.PontoTuristico;
 
-public class ScrollingFragment extends Fragment {
+public class ScrollingFragment extends Fragment implements View.OnClickListener {
 
     private Button buttonPernambuco, buttonNatal, buttonAngra, buttonAmazonas, buttonNoruega,
             buttonSaoPaulo;
     private Comunicador comunicador;
+    private PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.fundo_branco);
 
     @Override
     public void onAttach(Context context) {
@@ -40,75 +41,77 @@ public class ScrollingFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scrolling, container, false);
 
+        initViews(view);
+
+        startClicks(view);
+
+        return view;
+    }
+
+    public void initViews(View view) {
         buttonPernambuco = view.findViewById(R.id.buttonPernambuco);
         buttonNatal = view.findViewById(R.id.buttonNatal);
         buttonAngra = view.findViewById(R.id.buttonAngra);
         buttonAmazonas = view.findViewById(R.id.buttonAmazonia);
         buttonNoruega = view.findViewById(R.id.buttonNoruega);
         buttonSaoPaulo = view.findViewById(R.id.buttonSaoPaulo);
+    }
 
-        buttonPernambuco.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.praia_pernambuco);
+    public void startClicks(View view) {
+        buttonPernambuco.setOnClickListener(this);
+        buttonNatal.setOnClickListener(this);
+        buttonAngra.setOnClickListener(this);
+        buttonAmazonas.setOnClickListener(this);
+        buttonNoruega.setOnClickListener(this);
+        buttonSaoPaulo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonPernambuco:
+                pontoTuristico.setImagem(R.drawable.praia_pernambuco);
                 comunicador.comunicacao(pontoTuristico);
                 Toast.makeText(getContext(), "Você selecionou a praia de Pernambuco",
                         Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-
-        buttonNatal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.praia_natal);
+            case R.id.buttonNatal:
+                pontoTuristico.setImagem(R.drawable.praia_natal);
                 comunicador.comunicacao(pontoTuristico);
                 Toast.makeText(getContext(), "Você selecionou a praia de Natal",
                         Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-
-        buttonAngra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.praia_angra);
+            case R.id.buttonAngra:
+                pontoTuristico.setImagem(R.drawable.praia_angra);
                 comunicador.comunicacao(pontoTuristico);
                 Toast.makeText(getContext(), "Você selecionou a praia de Angra dos Reis",
                         Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-        buttonAmazonas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.rio_manaus);
+            case R.id.buttonAmazonia:
+                pontoTuristico.setImagem(R.drawable.rio_manaus);
                 comunicador.comunicacao(pontoTuristico);
                 Toast.makeText(getContext(), "Você selecionou os rios do Amazonas",
                         Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-        buttonNoruega.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.foto_noruega);
+            case R.id.buttonNoruega:
+                pontoTuristico.setImagem(R.drawable.foto_noruega);
                 comunicador.comunicacao(pontoTuristico);
                 Toast.makeText(getContext(), "Você selecionou as montanhas norueguesas",
                         Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-        buttonSaoPaulo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PontoTuristico pontoTuristico = new PontoTuristico(R.drawable.foto_sp);
+            case R.id.buttonSaoPaulo:
+                pontoTuristico.setImagem(R.drawable.foto_sp);
                 comunicador.comunicacao(pontoTuristico);
                 Toast.makeText(getContext(), "Você selecionou o parque Ibirapuera",
                         Toast.LENGTH_SHORT).show();
+                break;
 
-            }
-        });
-        return view;
+        }
     }
 }
